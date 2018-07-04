b124f1c8a1a5c7cdc7feae26133465c5754fc48b
Matěj Sychra <matej.sychra@t-mobile.cz>
2018-07-04 21:36:57 +0200

    synchronized sample keys, added node.js decryption example

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -9,7 +9,7 @@ char cleartext[256];
 char ciphertext[512];
 
 // AES Encryption Key
-byte aes_key[] = { 0x15, 0x2B, 0x7E, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
+byte aes_key[] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
 
 // General initialization vector (you must use your own IV's in production for full security!!!)
 byte aes_iv[N_BLOCK] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -18,13 +18,13 @@ byte aes_iv[N_BLOCK] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 void aes_init() {
   aesLib.gen_iv(aes_iv);
   // workaround for incorrect B64 functionality on first run...
-  encrypt("HELLO WORLD!", aes_iv); 
+  encrypt("HELLO WORLD!", aes_iv);
 }
 
-String encrypt(char * msg, byte iv[]) {  
+String encrypt(char * msg, byte iv[]) {
   int msgLen = strlen(msg);
   char encrypted[2 * msgLen];
-  aesLib.encrypt(msg, encrypted, aes_key, iv);  
+  aesLib.encrypt(msg, encrypted, aes_key, iv);
   return String(encrypted);
 }
 
@@ -32,7 +32,7 @@ String decrypt(char * msg, byte iv[]) {
   unsigned long ms = micros();
   int msgLen = strlen(msg);
   char decrypted[msgLen]; // half may be enough
-  aesLib.decrypt(msg, decrypted, aes_key, iv);  
+  aesLib.decrypt(msg, decrypted, aes_key, iv);
   return String(decrypted);
 }
 
@@ -45,8 +45,8 @@ void loop() {
 
   loopcount++;
 
-  sprintf(cleartext, "START; %i \n", loopcount);  
-  
+  sprintf(cleartext, "START; %i \n", loopcount);
+
   // Encrypt
   byte enc_iv[N_BLOCK] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // iv_block gets written to, provide own fresh copy...
   String encrypted = encrypt(cleartext, enc_iv);
@@ -56,9 +56,9 @@ void loop() {
 
   // Decrypt
   byte dec_iv[N_BLOCK] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // iv_block gets written to, provide own fresh copy...
-  String decrypted = decrypt(ciphertext, dec_iv);  
+  String decrypted = decrypt(ciphertext, dec_iv);
   Serial.print("Cleartext: ");
-  Serial.println(decrypted);  
+  Serial.println(decrypted);
 
   delay(500);
 }
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,22 @@
-{
-  "name": "aestest",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "",
-  "license": "ISC"
-}
+var CryptoJS = require("crypto-js");
+
+// Sample encrypted text "START; 380"
+var esp8266_msg = 'ei6NxsBeWk7hj41eia3S0Od26goTtxHvwO6V27LwSW4=';
+
+// null_iv[N_BLOCK] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+var esp8266_iv  = 'AAAAAAAAAAAAAAAAAAAAAA==';
+
+// 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C
+var AESKey = '2B7E151628AED2A6ABF7158809CF4F3C';
+
+var plain_iv =  new Buffer( esp8266_iv , 'base64').toString('hex');
+var iv = CryptoJS.enc.Hex.parse( plain_iv );
+var key= CryptoJS.enc.Hex.parse( AESKey );
+
+// Decrypt
+var bytes  = CryptoJS.AES.decrypt( esp8266_msg, key , { iv: iv } );
+var plaintext = bytes.toString(CryptoJS.enc.Base64);
+var decoded_b64msg =  new Buffer(plaintext , 'base64').toString('ascii');
+var decoded_msg =     new Buffer( decoded_b64msg , 'base64').toString('ascii');
+
+console.log("Decrypted message: ", decoded_msg);
