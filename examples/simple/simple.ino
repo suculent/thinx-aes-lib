f97302a7080cc55f8511b025ff26d843003d5152
igraczech <suculent@me.com>
2019-11-04 00:01:53 +0100

    examples adjusted to recent PRs

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -28,7 +28,7 @@ String encrypt(char * msg, byte iv[]) {
   int msgLen = strlen(msg);
   Serial.print("msglen = "); Serial.println(msgLen);
   char encrypted[4 * msgLen]; // AHA! needs to be large, 2x is not enough
-  aesLib.encrypt64(msg, encrypted, aes_key, iv);
+  aesLib.encrypt64(msg, encrypted, aes_key, sizeof(aes_key), iv);
   Serial.print("encrypted = "); Serial.println(encrypted);
   return String(encrypted);
 }
@@ -37,7 +37,7 @@ String decrypt(char * msg, byte iv[]) {
   unsigned long ms = micros();
   int msgLen = strlen(msg);
   char decrypted[msgLen]; // half may be enough
-  aesLib.decrypt64(msg, decrypted, aes_key, iv);
+  aesLib.decrypt64(msg, decrypted, aes_key, sizeof(aes_key), iv);
   return String(decrypted);
 }
 
