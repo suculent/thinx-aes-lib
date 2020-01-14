10ac43e3a3ede4dccb07964f8d9c63e6e98f935e
Matej Sychra <suculent@me.com>
2020-01-14 23:43:50 +0100

    added proof-of-work on Arduino UNO with 128 +1 byte buffer limit and 128-byte payload (may be pushed more)

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,4 +1,4 @@
-/* Minimalistic example for Readme with minimum load (< 256 bytes) */
+/* Simple buffer example for low-memory conditions (Arduino) */
 
 #include "AESLib.h"
 
@@ -6,18 +6,12 @@
 
 AESLib aesLib;
 
-#define INPUT_BUFFER_LIMIT 256
+#define INPUT_BUFFER_LIMIT (128 + 1) // designed for Arduino UNO, not stress-tested anymore (this works with readBuffer[129])
 
 char cleartext[INPUT_BUFFER_LIMIT] = {0}; // THIS IS INPUT BUFFER (FOR TEXT)
 char ciphertext[2*INPUT_BUFFER_LIMIT] = {0}; // THIS IS OUTPUT BUFFER (FOR BASE64-ENCODED ENCRYPTED DATA)
 
-// 887 bytes (<1K)
-// String readBuffer = "123456789123456789B123456789C123456789D123456789E123456789F123456789G0064123456789A123456789B123456789C123456789D123456789E123456789F123456789G0128123456789A123456789B123456789C123456789D123456789E123456789F123456789G0064123456789A123456789B123456789C123456789D123456789E123456789F123456789G0296123456789A123456789B123456789C123456789D123456789E123456789F123456789G0064123456789A123456789B123456789C123456789D123456789E123456789F123456789G0128123456789A123456789B123456789C123456789D123456789E123456789F123456789G0064123456789A123456789B123456789C123456789D123456789E123456789F123456789G0296123456789A123456789B123456789C123456789D123456789E123456789F123456789G0064123456789A123456789B123456789C123456789D123456789E123456789F123456789G0128123456789A123456789B123456789C123456789D123456789E123456789F123456789G0064123456789A123456789B123456789C123456789D123456789E123456789F123456789G\n887";
-
-// 1072 bytes (~1K) works with 2048K+4096K buffer
-String readBuffer = "HELLO WORLD!";
-
-// 1522 bytes does not work with 3072K buffers, crashes because of dual base64... use standard encrypt() instead; see large example
+char readBuffer[129] = "1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF\0";
 
 // AES Encryption Key (same as in node-js example)
 byte aes_key[] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
@@ -31,21 +25,17 @@ void aes_init() {
 }
 
 uint16_t encrypt_to_ciphertext(char * msg, byte iv[]) {
+  Serial.println("Calling encrypt (string)...");
   int msgLen = strlen(msg);
   int cipherlength = aesLib.get_cipher64_length(msgLen);
-  char encrypted_bytes[cipherlength];
-  uint16_t enc_length = aesLib.encrypt64(msg, encrypted_bytes, aes_key, sizeof(aes_key), iv);
-  sprintf(ciphertext, "%s", encrypted_bytes);
-  return enc_length;
+  aesLib.encrypt(msg, ciphertext, aes_key, sizeof(aes_key), iv);
+  return cipherlength;
 }
 
 void decrypt_to_cleartext(char * msg, uint16_t msgLen, byte iv[]) {
-  Serial.println("Calling decrypt64...");
-#ifdef ESP8266
-  //Serial.print("[decrypt_to_cleartext] free heap: "); Serial.println(ESP.getFreeHeap());
-#endif
-  uint16_t decLen = aesLib.decrypt64(msg, cleartext, aes_key, sizeof(aes_key), iv);
-  Serial.print("Decrypted bytes: "); Serial.println(decLen);
+  Serial.print("Calling decrypt...; ");
+  aesLib.decrypt(msg, msgLen, cleartext, aes_key, sizeof(aes_key), iv);
+  Serial.print("Decrypted bytes: "); Serial.println(strlen(cleartext));
 }
 
 void setup() {
@@ -55,11 +45,6 @@ void setup() {
 
   aes_init(); // generate random IV, should be called only once? causes crash if repeated...
 
-#ifdef ESP8266
-  Serial.print("[setup] free heap: "); Serial.println(ESP.getFreeHeap());
-#endif
-
-  //Serial.println("Enter text to be encrypted into console (no feedback) and press ENTER (newline):");
 }
 
 /* non-blocking wait function */
@@ -77,33 +62,27 @@ byte enc_iv_from[N_BLOCK] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 void loop() {
 
-#ifdef ESP8266
-  Serial.print("»» LOOP BEGINS. Free heap: "); Serial.println(ESP.getFreeHeap());
-#endif
-
-  Serial.print("readBuffer length: "); Serial.println(readBuffer.length());
+  Serial.print("readBuffer length: "); Serial.println(strlen(readBuffer));
 
    // must not exceed INPUT_BUFFER_LIMIT bytes; may contain a newline
-  sprintf(cleartext, "%s", readBuffer.c_str());
+  sprintf(cleartext, "%s", readBuffer);
 
   // Encrypt
   // iv_block gets written to, provide own fresh copy... so each iteration of encryption will be the same.
   uint16_t len = encrypt_to_ciphertext(cleartext, enc_iv_to);
   Serial.print("Encrypted length = "); Serial.println(len);
+  memset(enc_iv_to, 0, sizeof(enc_iv_to));
 
-  Serial.println("Encrypted. Decrypting..."); Serial.flush();
+  Serial.println("Encrypted. Decrypting..."); Serial.println(len); Serial.flush();
   decrypt_to_cleartext(ciphertext, len, enc_iv_from);
   Serial.print("Decrypted cleartext:\n"); Serial.println(cleartext);
+  memset(enc_iv_from, 0, sizeof(enc_iv_from));
 
-  if (readBuffer == String(cleartext)) {
+  if (strcmp(readBuffer, cleartext) == 0) {
     Serial.println("Decrypted correctly.");
   } else {
     Serial.println("Decryption test failed.");
   }
-
-#ifdef ESP8266
-  Serial.print("»» LOOP ENDED. Free heap: "); Serial.println(ESP.getFreeHeap());
-#endif
 
   Serial.println("---");
 
