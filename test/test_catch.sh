74d0a58dd0910a9cc3e654a1102919f64ee98092
Matej Sychra <suculent@me.com>
2020-01-24 11:23:52 +0100

    added sample test coverage

--- test/test_catch.sh
+++ test/test_catch.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-# Build test-cases and test with Check2
+# Build test-cases and test with Check2 and coverage
 
-g++ -std=c++14 -Wall -o 020-TestCase 020-TestCase.cpp ../src/*.cpp && ./020-TestCase --success
+g++ -fprofile-arcs -ftest-coverage -std=c++14 -Wall -o 020-TestCase 020-TestCase.cpp ../src/*.cpp
+
+pushd coverage
+rm -rf
+
+# Collect coverage
+
+gcov ../020-TestCase
+
+popd
