bcd607885edd8444bb49602d74dc73f492c8053c
Matej Sychra <suculent@me.com>
2020-01-24 11:52:26 +0100

    added static sonarcloud.io build wrapper

--- test/test_catch.sh
+++ test/test_catch.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-# Build test-cases and test with Check2 and coverage
+# Build test-cases and test with Check2 and coverage using Sonarcloud.io wrapper
 
+if [[ $(uname) == "Darwin" ]]; then
+  BUILD_WRAPPER="../build-wrapper-macosx-x86/build-wrapper-macosx-x86"
+else
+  BUILD_WRAPPER="../build-wrapper-linux-x86/build-wrapper-linux-x86"
+fi
+
+${BUILD_WRAPPER} \
+--out-dir build_wrapper_output_directory \
 g++ -fprofile-arcs -ftest-coverage -std=c++14 -Wall -o 020-TestCase 020-TestCase.cpp ../src/*.cpp
 
 pushd coverage
